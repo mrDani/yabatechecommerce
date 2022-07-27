b177081fda06ef157e13e1502b6a3bcc7744017f
Danny <Darnnielnology@gmail.com>
2022-07-27 12:43:51 +0100

    new changes

--- backend/settings.py
+++ backend/settings.py
@@ -122,13 +122,18 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'todoappdb',
-        'USER': 'postgres',
-        'PASSWORD': 'babalola',
-        'HOST': 'localhost',
-        'PORT': '5432'
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'todoappdb',
+#         'USER': 'postgres',
+#         'PASSWORD': 'babalola',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+# }
+
+'default': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': BASE_DIR / 'db.sqlite3',
 }
  }
 
